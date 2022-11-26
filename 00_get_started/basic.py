71cea41e61022a0ecb5d75014599bf46b2b0c59e
Sabin Purice <sabin.purice@gmail.com>
2022-11-26 19:19:16 +0200

    basic to the latest version

--- 00_get_started/basic.py
+++ 00_get_started/basic.py
@@ -1,29 +1,28 @@
-# -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
 
-app = dash.Dash()
+app = Dash(__name__)
 
-colors = { 'background' : '#111111'
-         , 'text'       : '#7FDBFF'
-         }
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame(
+    {
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+    }
+)
 
-app.layout = html.Div( style={'backgroundColor': colors['background']}, children=[
-    html.H1(children='Hello Dash', style={'textAlign':'center', 'color':colors['text']}),
-    html.Div(children='Dash: A web application framework for Python.', style={'textAlign':'center', 'color':colors['text']}),
-    dcc.Graph ( id='example-graph'
-              , figure={ 'data': [ {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                                   {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                                 ]
-                       , 'layout': { 'title'         : 'Dash Data Visualization'
-                                   , 'plot_bgcolor'  : colors['background']
-                                   , 'paper_bgcolor' : colors['background']
-                                   , 'font'          : {'color': colors['text']}
-                                   }
-                       }
-              )
-    ])
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+app.layout = html.Div(
+    children=[
+        html.H1(children="Hello Dash"),
+        html.Div(children="Dash: A web application framework for your data."),
+        dcc.Graph(id="example-graph", figure=fig),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run(debug=True)
