c015b9e348480256ad1d64389857d3b11a028da5
Sabin Purice <sabin.purice@gmail.com>
2022-11-26 22:51:57 +0200

    Create 7.comps_instead_id.py

---
+++ 01_callbacks/7.comps_instead_id.py
@@ -0,0 +1,32 @@
+from dash import Dash, dcc, html, Input, Output, State
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    [
+        html.H4("Change the value in the text box to see callbacks in action!"),
+        html.Div(
+            [
+                "Input: ",
+                my_input := dcc.Input(value="initial value", type="text"),
+            ]
+        ),
+        html.Br(),
+        my_output := html.Div(),
+    ],
+    style={"fontFamily": "monospace"},
+)
+
+
+@app.callback(
+    Output(my_output, component_property="children"),
+    Input(my_input, component_property="value"),
+)
+def update_output_div(input_value):
+    return f"Output: {input_value}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
