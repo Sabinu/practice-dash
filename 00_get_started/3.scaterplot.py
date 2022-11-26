430dc8a07ce630e6e2ada941c33ccf21bdb6b273
Sabin Purice <sabin.purice@gmail.com>
2022-11-26 20:14:25 +0200

    update scatterplot

--- 00_get_started/3.scaterplot.py
+++ 00_get_started/3.scaterplot.py
@@ -1,8 +1,8 @@
 from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
-import plotly.graph_objs as go
 
-app = Dash()
+app = Dash(__name__)
 
 df = pd.read_csv(
     "https://gist.githubusercontent.com/chriddyp/"
@@ -11,36 +11,22 @@ df = pd.read_csv(
     + "gdp-life-exp-2007.csv"
 )
 
+fig = px.scatter(
+    df,
+    x="gdp per capita",
+    y="life expectancy",
+    size="population",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
 
 app.layout = html.Div(
     [
-        dcc.Graph(
-            id="life-exp-vs-gdp",
-            style={"height": "800px"},
-            figure={
-                "data": [
-                    go.Scatter(
-                        x=df[df["continent"] == i]["gdp per capita"],
-                        y=df[df["continent"] == i]["life expectancy"],
-                        text=df[df["continent"] == i]["country"],
-                        mode="markers",
-                        opacity=0.7,
-                        marker={"size": 15, "line": {"width": 0.5, "color": "white"}},
-                        name=i,
-                    )
-                    for i in df.continent.unique()
-                ],
-                "layout": go.Layout(
-                    xaxis={"type": "log", "title": "GDP Per Capita"},
-                    yaxis={"title": "Life Expectancy"},
-                    margin={"l": 40, "b": 40, "t": 10, "r": 10},
-                    legend={"x": 0, "y": 1},
-                    hovermode="closest",
-                ),
-            },
-        )
+        dcc.Graph(id="life-exp-vs-gdp", figure=fig),
     ]
 )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
