0285a180461d4e15341c740f6349b3cec5d58f19
Sabin Purice <sabin.purice@gmail.com>
2022-11-27 18:43:41 +0200

    nice tailwind spacing

--- 3.beyond_basics/4.multi_page/pages/analytics.py
+++ 3.beyond_basics/4.multi_page/pages/analytics.py
@@ -9,9 +9,13 @@ layout = html.Div(
             [
                 "Select a city: ",
                 dcc.RadioItems(
-                    ["New York City", "Montreal", "San Francisco"],
+                    [
+                        {"value": i, "label": html.Span(i, className="pl-2")}
+                        for i in ["New York City", "Montreal", "San Francisco"]
+                    ],
                     "Montreal",
                     id="analytics-input",
+                    className="space-x-4",
                 ),
             ]
         ),
